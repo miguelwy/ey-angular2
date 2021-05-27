16acdeda9da051ed83213b739cff60b5a2467d97
Wychovaniec <223017978@ge.com>
2021-05-27 09:31:15 -0300

    'update'

---
+++ src/app/table-list/table-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-list',
+  templateUrl: './table-list.component.html',
+  styleUrls: ['./table-list.component.css']
+})
+export class TableListComponent implements OnInit {
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+    let userList : Array<string> = [ "e"]
+  }
+
+}
